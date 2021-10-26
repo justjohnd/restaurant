ed6fbd2e1ee9b25bcdc9bf735db4a8068f3be541
johnd <justjohnd@gmail.com>
2021-10-26 11:18:28 +0900

    Finish draft site, database connected, articles and comments associated

---
+++ app/javascript/packs/hamburger.js
@@ -0,0 +1,24 @@
+//Mobile hamburger
+const hamburger = document.querySelector('.hamburger');
+const lines = document.querySelectorAll('.line');
+const hamburgerClose = document.querySelector('.hamburger-close');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburgerClose.classList.toggle('toggle');
+  lines.forEach((line) => {
+    line.classList.toggle('toggle');
+  });
+
+  if (mobileMenu.classList.contains('d-none')) {
+    mobileMenu.classList.replace('d-none', 'd-flex');
+  }
+
+  if (mobileMenu.classList.contains('menu-visible')) {
+    mobileMenu.classList.replace('menu-visible', 'menu-hidden');
+  } else if (mobileMenu.classList.contains('menu-hidden')) {
+    mobileMenu.classList.replace('menu-hidden', 'menu-visible');
+  } else {
+    mobileMenu.classList.add('menu-visible');
+  }
+});
