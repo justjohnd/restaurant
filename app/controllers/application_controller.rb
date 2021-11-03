e57667c0bcb91f0bb3d8ec13e1682e89ab73cd3e
johnd <justjohnd@gmail.com>
2021-11-03 09:18:45 +0900

    Configure french, english pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+before_action :set_locale
+
+  def set_locale
+    if user_signed_in?
+      I18n.locale = current_user.language
+    else
+      I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+    end
+  end
+
+  def locale_from_header
+    request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
+  end
+
+  def default_url_options
+  { lang: I18n.locale }
+end
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_015555) do
+ActiveRecord::Schema.define(version: 2021_11_02_092235) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_10_28_015555) do
     t.string "nameLast"
     t.string "nameFirst"
     t.string "userName"
+    t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["userName"], name: "index_users_on_username", unique: true
