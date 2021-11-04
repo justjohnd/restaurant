a873cdbda45de28f60f299a35174cd64279ca2a3
johnd <justjohnd@gmail.com>
2021-11-04 14:12:35 +0900

    Create new reservations page and add social links

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
   get "pages/menu", :as => "menu_page"
   get "pages/about", :as => "about_page"
-  get "pages/reservations", :as => "reservations_page"
+  get "contacts/new", :as => "contact_page"
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
